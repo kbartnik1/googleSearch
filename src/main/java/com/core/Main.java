0e5c6925bd5f256dec9194ec4434176b7bc2b532
KRBK <krbk@gft.com>
2018-04-13 15:31:02 +0200

    Tests are divided per one executor that (they) run simultaneously

--- src/main/java/com/core/Main.java
+++ src/main/java/com/core/Main.java
@@ -5,13 +5,11 @@
  */
 package com.core;
 
-import com.tests.TestManager;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -23,33 +21,17 @@ import java.util.List;
 public class Main {
 
     private static final Logger log = Logger.getLogger(Main.class);
-    public static List<String> methodNames = new ArrayList<>();
 
-    public static void main(String[] args) throws MalformedURLException {
-        Main m = new Main();
+
+    public static void main(String[] args){
         try {
             PropertyConfigurator.configure(new FileInputStream("src\\main\\resources\\log4j.properties"));
             log.info("Property files located and loaded");
         } catch (FileNotFoundException ex) {
             log.error("Property files not found. " + ex.getMessage());
         }
-        getTestMethodNames();
-
         TestNGExecutor testNGExecutor = new TestNGExecutor();
         testNGExecutor.runTests();
     }
-
-
-    public static void getTestMethodNames() {
-        Class c = TestManager.class;
-        Method[] m = c.getDeclaredMethods();
-        for (int i = 0; i < m.length; i++) {
-            methodNames.add(m[i].toString());
-            String tmpName = methodNames.get(i);
-            String[] tmp = tmpName.split("\\.");
-            methodNames.set(i, tmp[tmp.length - 1].substring(0, tmp[tmp.length - 1].length() - 2));
-        }
-    }
-
 
 }
